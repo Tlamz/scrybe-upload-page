496e6f1aefd75d67e12565fc278f14f484ffcc3a
Tosin Oyelami <tosin_oyelami@yahoo.com>
2022-11-18 21:06:38 +0100

    feat: Upload pages modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import { Home } from "./pages/Home.js";
+// import "./pages/Place.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BlankPage } from "./pages/blank.page";
+// import { Place } from "./pages/Place.js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<BlankPage/>} />
+          {/* <Route path="/place" element={<Place />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/blank.page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+import {Modal} from "../components/modal.component"
+
+export function BlankPage () {
+const [showModal, setShowModal] = useState(true);
+  return <>
+   <button onClick={() => setShowModal(true)}>Show Modal</button>
+  {showModal && <Modal closeModal={setShowModal} />}
+  </>;
+}
